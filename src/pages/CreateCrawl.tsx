30abdfe1b2b68942610cd205e93d4dfd4b9e2ac7
Swan-17 <matthewswan17@gmail.com>
2026-08-04 12:46:35 +0100

    Add create crawl form interaction

--- src/pages/CreateCrawl.tsx
+++ src/pages/CreateCrawl.tsx
@@ -1,13 +1,105 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function CreateCrawl() {
+  const [crawlName, setCrawlName] = useState('')
+  const [location, setLocation] = useState('')
+  const [numberOfPubs, setNumberOfPubs] = useState(5)
+  const [created, setCreated] = useState(false)
+
+  function handleCreateCrawl() {
+    setCreated(true)
+  }
+
   return (
     <div>
       <h1>Create Crawl 🍻</h1>
 
+      <label>
+        Crawl name:
+      </label>
+
+      <br />
+
+      <input
+        type="text"
+        value={crawlName}
+        onChange={(event) => setCrawlName(event.target.value)}
+      />
+
+      <br /><br />
+
+      <label>
+        Starting location:
+      </label>
+
+      <br />
+
+      <input
+        type="text"
+        value={location}
+        onChange={(event) => setLocation(event.target.value)}
+      />
+
+      <br /><br />
+
+      <label>
+        Number of pubs:
+      </label>
+
+      <br />
+
+      <input
+        type="number"
+        value={numberOfPubs}
+        onChange={(event) =>
+          setNumberOfPubs(Number(event.target.value))
+        }
+      />
+
+      <br /><br />
+
+      <button onClick={handleCreateCrawl}>
+        Create Crawl
+      </button>
+
+      <hr />
+
+      <h3>Current Details</h3>
+
       <p>
-        Build your perfect pub route.
+        Crawl name: {crawlName}
       </p>
+
+      <p>
+        Starting location: {location}
+      </p>
+
+      <p>
+        Number of pubs: {numberOfPubs}
+      </p>
+
+      {created && (
+        <div>
+          <h2>
+            Crawl created! 🎉
+          </h2>
+
+          <p>
+            Your crawl "{crawlName}" has been planned.
+          </p>
+
+          <p>
+            Starting at: {location}
+          </p>
+
+          <p>
+            Pubs planned: {numberOfPubs}
+          </p>
+        </div>
+      )}
+
+      <br />
 
       <Link to="/">
         Back Home
@@ -16,5 +108,4 @@ function CreateCrawl() {
   )
 }
 
-export default CreateCrawl
- 
+export default CreateCrawl 
