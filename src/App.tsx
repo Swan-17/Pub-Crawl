8a7bc9b2e81b8cd49dc9b8fc793a3d33e953d176
Swan-17 <matthewswan17@gmail.com>
2026-08-04 12:20:38 +0100

    Create initial page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  return (
-    <div>
-      <h1>Pub Crawl</h1>
-      <p>Your pub adventure starts here.</p>
-    </div>
-  )
+  return <Home />
 }
 
 export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <div>
+      <h1>Pub Crawl 🍻</h1>
+
+      <p>
+        Plan the perfect pub crawl with your friends.
+      </p>
+
+      <button>
+        Create Crawl
+      </button>
+
+      <button>
+        Join Crawl
+      </button>
+    </div>
+  )
+}
+
+export default Home
